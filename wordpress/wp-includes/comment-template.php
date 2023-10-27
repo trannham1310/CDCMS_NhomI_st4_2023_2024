fd1253c175777e93d1bbaeb2a9d25060436865b4
giahuybui <92190458+giahuybui@users.noreply.github.com>
2023-10-27 22:56:00 +0700

    chinh sua module 8

--- wordpress/wp-includes/comment-template.php
+++ wordpress/wp-includes/comment-template.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
 <?php
 /**
  * Comment template functions
@@ -2499,7 +2501,7 @@ function comment_form( $args = array(), $post = null ) {
 	$checked_attribute  = ( $html5 ? ' checked' : ' checked="checked"' );
 
 	// Identify required fields visually and create a message about the indicator.
-	$required_indicator = ' ' . wp_required_field_indicator();
+	$required_indicator = ' ' ;//. wp_required_field_indicator();
 	$required_text      = ' ' . wp_required_field_message();
 
 	$fields = array(
@@ -2577,13 +2579,13 @@ function comment_form( $args = array(), $post = null ) {
 	$defaults = array(
 		'fields'               => $fields,
 		'comment_field'        => sprintf(
-			'<p class="comment-form-comment 111111">%s %s</p>',
+			'<p class="comment-form-comment">%s %s</p>',
 			sprintf(
-				'<label for="comment">%s%s</label>',
-				_x( 'Comment', 'noun' ),
+				// '<label for="comment">%s%s</label>',
+				// _x( 'Comment', 'noun' ),
 				$required_indicator
 			),
-			'<textarea id="comment" name="comment" cols="45" rows="8" maxlength="65525"' . $required_attribute . '></textarea>'
+			'<textarea style="border: 1px solid #ced4da !important;" class="form-control" id="comment" name="comment" rows="3" maxlength="65525" placeholder="What are you thinking..."' . $required_attribute . '></textarea>'
 		),
 		'must_log_in'          => sprintf(
 			'<p class="must-log-in">%s</p>',
@@ -2817,7 +2819,37 @@ function comment_form( $args = array(), $post = null ) {
 
 							echo $args['comment_notes_after'];
 						} else {
-							dynamic_sidebar( 'comment-8' );
+							echo '
+							<!--- Post Form Begins -->
+							<section class="card">
+								<div class="card-header">
+									<ul class="nav nav-tabs card-header-tabs" id="myTab" role="tablist">
+										<li class="nav-item">
+											<a class="nav-link active" id="posts-tab" data-toggle="tab" href="#posts" role="tab" aria-controls="posts" aria-selected="true">Make
+												a Post</a>
+										</li>
+									</ul>
+								</div>
+								<div class="card-body">
+									<div class="tab-content" id="myTabContent">
+										<div class="tab-pane fade show active" id="posts" role="tabpanel" aria-labelledby="posts-tab">
+											<div class="form-group">						
+												'. apply_filters( 'comment_form_field_comment', $field ) .'
+											</div>
+
+										</div>
+									</div>
+									<div class="text-right" style="height: 40px">
+										<button style="background: #007bff" type="submit" class="btn btn-primary">share</button>
+										<div style="visibility: hidden">
+										'. apply_filters( 'comment_form_submit_field', $submit_field, $args ) .'
+										</div>
+									</div>
+								</div>
+							</section>
+							<!--- Post Form Ends -->
+							';
+							echo $args['comment_notes_after'];
 						}
 
 					} elseif ( ! is_user_logged_in() ) {
