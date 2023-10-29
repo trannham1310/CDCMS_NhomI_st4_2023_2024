301e30e3c390df867699fb014773bb47af44ae57
giahuybui <92190458+giahuybui@users.noreply.github.com>
2023-10-29 23:40:05 +0700

    css

--- wordpress/wp-content/themes/twentytwentyone/index.php
+++ wordpress/wp-content/themes/twentytwentyone/index.php
@@ -23,91 +23,137 @@ get_header(); ?>
 	</header><!-- .page-header -->
 <?php endif; ?>
 <style>
-	.column {
-		width: 10%;
-		float: left;
+	.number-top-view {
+		font-family: "Merriweather", serif;
+		font-size: 48px;
+		color: #222;
+		font-weight: bold;
 	}
-</style>
+
+	.item-news {
+		padding-bottom: 20px;
+		border-bottom: 1px solid #e5e5e5;
+	}
+
+	.item-news .title-news {
+		font-family: "Merriweather", serif;
+		font-size: 14px;
+		display: inline;
+	}
+
+	.post-11 .title-news a {
+		text-decoration: none;
+		color: #000;
+		padding-left: 10px;
+	}
+	.post-11 {
+		border-top: 1px solid #e5e5e5;
+	}
+
+	.post-view-11:before {
+		content: "";
+		width: 1px;
+		height: 200px;
+		position: absolute;
+		top: 40px;
+		left: 50%;
+		background: #e5e5e5;
+	}
+	.post-title-11 {
+		display: inline-block;
+		position: relative;
+	}
+	.post-title-11:before {
+		content: "";
+		width: 100%;
+		height: 1px;
+		background: #9f224e;
+		position: absolute;
+		left: 0;
+		bottom: -8px;
+	}
+	</style>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <div class="container">
 	<div class="row">
-		<div class="col-md-4 ">
-			<div class="row ">
-				<h4 class="post">Bài viết yêu thích</h4>
-				<div class="column">
-					<?php
-					// Query để lấy danh sách bài viết
-					$query = new WP_Query();
-					$query->query('posts_per_page=3');
+		<div class="col-md-4 position-relative">
+			<h4 class="post-title-11">Bài viết yêu thích</h4>
+			<article class="item-news">
+				<div class="row post-view-11">
+					<div class="col-md-6">
+						<?php
+						// Query để lấy danh sách bài viết
+						$query = new WP_Query();
+						$query->query('posts_per_page=3');
 
-					// Số thứ tự bài viết
-					$count = 1;
+						// Số thứ tự bài viết
+						$count = 1;
 
-					while ($query->have_posts()) : $query->the_post(); ?>
-						<div>
-							<span><?php echo $count; ?></span>
-							<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-						</div>
-					<?php
-						// Tăng số thứ tự bài viết lên 1
-						$count++;
-					endwhile;
+						while ($query->have_posts()) : $query->the_post(); ?>
+							<div class="post-11">
+								<span class="number-top-view"><?php echo $count; ?></span>
+								<h3 class="title-news"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+							</div>
+						<?php
+							// Tăng số thứ tự bài viết lên 1
+							$count++;
+						endwhile;
 
-					// Đặt lại query
-					wp_reset_query();
-					?>
+						// Đặt lại query
+						wp_reset_query();
+						?>
+					</div>
+
+					<div class="col-md-6">
+						<?php
+						// Query để lấy danh sách bài viết
+						$query = new WP_Query();
+						$query->query('posts_per_page=3&offset=3');
+
+						// Số thứ tự bài viết
+						$count = 4;
+
+						while ($query->have_posts()) : $query->the_post(); ?>
+							<div class="post-11">
+								<span class="number-top-view"><?php echo $count; ?></span>
+								<h3 class="title-news"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+							</div>
+						<?php
+							// Tăng số thứ tự bài viết lên 1
+							$count++;
+						endwhile;
+
+						// Đặt lại query
+						wp_reset_query();
+						?>
+					</div>
 				</div>
+			</article>
+		</div>	
+		<div class="col-md-4">
+			<?php
+			if (have_posts()) {
 
-				<div class="column">
-					<?php
-					// Query để lấy danh sách bài viết
-					$query = new WP_Query();
-					$query->query('posts_per_page=3&offset=3');
+				// Load posts loop.
+				while (have_posts()) {
+					the_post();
 
-					// Số thứ tự bài viết
-					$count = 4;
+					get_template_part('template-parts/content/content', get_theme_mod('display_excerpt_or_full_post', 'excerpt'));
+				}
 
-					while ($query->have_posts()) : $query->the_post(); ?>
-						<div>
-							<span><?php echo $count; ?></span>
-							<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-						</div>
-					<?php
-						// Tăng số thứ tự bài viết lên 1
-						$count++;
-					endwhile;
+				// Previous/next page navigation.
+				twenty_twenty_one_the_posts_navigation();
+			} else {
 
-					// Đặt lại query
-					wp_reset_query();
-					?>
-				</div>
-			</div>
+				// If no content, include the "No posts found" template.
+				get_template_part('template-parts/content/content-none');
+			}
+			?>
 		</div>
+		<div class="col-md-4">
+
+		</div>	
 	</div>
-</div>
-<div class="col-md-4">
-	<?php
-	if (have_posts()) {
-
-		// Load posts loop.
-		while (have_posts()) {
-			the_post();
-
-			get_template_part('template-parts/content/content', get_theme_mod('display_excerpt_or_full_post', 'excerpt'));
-		}
-
-		// Previous/next page navigation.
-		twenty_twenty_one_the_posts_navigation();
-	} else {
-
-		// If no content, include the "No posts found" template.
-		get_template_part('template-parts/content/content-none');
-	}
-	?>
-</div>
-<div class="col-md-4">
-
-</div>
-</div>
 </div>
 
 <?php
