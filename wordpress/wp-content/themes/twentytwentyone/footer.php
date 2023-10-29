550734a7f7999f1aa73359e56a312f8740669fc9
Nhatleo <120942667+Nhatleo@users.noreply.github.com>
2023-10-29 17:50:52 +0700

    80%

--- wordpress/wp-content/themes/twentytwentyone/footer.php
+++ wordpress/wp-content/themes/twentytwentyone/footer.php
@@ -16,7 +16,8 @@
 		</div><!-- #primary -->
 	</div><!-- #content -->
 
-	<?php get_template_part( 'template-parts/footer/footer-widgets' ); ?>
+	<!-- <?php get_template_part( 'template-parts/footer/footer-widgets' ); ?>  -->
+	
 	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -98,6 +99,9 @@ section .section-title {
     padding-bottom: 0px;
     margin-bottom: 10px;
 }
+.container a{
+	font-size: 1rem !important;
+}
 }
 </style>
 	<section id="footer">
@@ -105,51 +109,27 @@ section .section-title {
 			<div class="row text-center text-xs-center text-sm-left text-md-left">
 				<div class="col-xs-12 col-sm-4 col-md-4">
 					<h5>Comment</h5>
-					<ul class="list-unstyled quick-links">
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Home</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>About</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Videos</a></li>
+					<ul class="list-unstyled quick-links" >
+						<li><a href="https://www.fiverr.com/share/qb8D02"><?php dynamic_sidebar( 'footer-1' ); ?></a></li>
 					</ul>
 				</div>
 				<div class="col-xs-12 col-sm-4 col-md-4">
-					<h5>Categories</h5>
-					<?php
-					// var_dump(get_the_category_list());
-					?>
+				    <h5>Categories</h5>
 					<ul class="list-unstyled quick-links">
-					<?php 
-						$categories_list = get_the_category_list( wp_get_list_item_separator() );
-						$categorys = explode(", ", $categories_list);
-						foreach ($categorys as $category) {
-							echo '<li>'. $category .'</li>';
-						}
-					?>
+						<li><a href="https://www.fiverr.com/share/qb8D02"><?php dynamic_sidebar( 'footer-2' ); ?></a></li>
 					</ul>
-					<!-- <ul class="list-unstyled quick-links">
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Home</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>About</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Videos</a></li>
-					</ul> -->
 
 				</div>
 				<div class="col-xs-12 col-sm-4 col-md-4">
 					<h5>Last Post</h5>
 					<ul class="list-unstyled quick-links">
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Home</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>About</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
-						<li><a href="https://wwwe.sunlimetech.com" title="Design and developed by"><i class="fa fa-angle-double-right"></i>Imprint</a></li>
+						<li><a href="https://www.fiverr.com/share/qb8D02"><?php dynamic_sidebar( 'footer-3' ); ?></a></li>
 					</ul>
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
-					<ul class="list-unstyled list-inline social text-center">
+					<ul class="list-unstyled list-inline social text-center" style="margin-top: -50px;">
 						<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-facebook"></i></a></li>
 						<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-twitter"></i></a></li>
 						<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-instagram"></i></a></li>
