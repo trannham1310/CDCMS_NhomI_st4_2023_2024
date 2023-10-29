c951a4aaeca6d9b293dc80335c236a416c6f7330
Nhatleo <120942667+Nhatleo@users.noreply.github.com>
2023-10-29 17:08:35 +0700

    50%

--- wordpress/wp-content/themes/twentytwentyone/index.php
+++ wordpress/wp-content/themes/twentytwentyone/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The main template file
  *
@@ -16,30 +17,99 @@
 
 get_header(); ?>
 
-<?php if ( is_home() && ! is_front_page() && ! empty( single_post_title( '', false ) ) ) : ?>
+<?php if (is_home() && !is_front_page() && !empty(single_post_title('', false))) : ?>
 	<header class="page-header alignwide">
 		<h1 class="page-title"><?php single_post_title(); ?></h1>
 	</header><!-- .page-header -->
 <?php endif; ?>
+<style>
+	.column {
+		width: 10%;
+		float: left;
+	}
+</style>
+<div class="container">
+	<div class="row">
+		<div class="col-md-4 ">
+			<div class="row ">
+				<h4 class="post">Bài viết yêu thích</h4>
+				<div class="column">
+					<?php
+					// Query để lấy danh sách bài viết
+					$query = new WP_Query();
+					$query->query('posts_per_page=3');
+
+					// Số thứ tự bài viết
+					$count = 1;
+
+					while ($query->have_posts()) : $query->the_post(); ?>
+						<div>
+							<span><?php echo $count; ?></span>
+							<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+						</div>
+					<?php
+						// Tăng số thứ tự bài viết lên 1
+						$count++;
+					endwhile;
+
+					// Đặt lại query
+					wp_reset_query();
+					?>
+				</div>
+
+				<div class="column">
+					<?php
+					// Query để lấy danh sách bài viết
+					$query = new WP_Query();
+					$query->query('posts_per_page=3&offset=3');
+
+					// Số thứ tự bài viết
+					$count = 4;
+
+					while ($query->have_posts()) : $query->the_post(); ?>
+						<div>
+							<span><?php echo $count; ?></span>
+							<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+						</div>
+					<?php
+						// Tăng số thứ tự bài viết lên 1
+						$count++;
+					endwhile;
+
+					// Đặt lại query
+					wp_reset_query();
+					?>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="col-md-4">
+	<?php
+	if (have_posts()) {
+
+		// Load posts loop.
+		while (have_posts()) {
+			the_post();
+
+			get_template_part('template-parts/content/content', get_theme_mod('display_excerpt_or_full_post', 'excerpt'));
+		}
+
+		// Previous/next page navigation.
+		twenty_twenty_one_the_posts_navigation();
+	} else {
+
+		// If no content, include the "No posts found" template.
+		get_template_part('template-parts/content/content-none');
+	}
+	?>
+</div>
+<div class="col-md-4">
+
+</div>
+</div>
+</div>
 
 <?php
-if ( have_posts() ) {
-
-	// Load posts loop.
-	while ( have_posts() ) {
-		the_post();
-
-		get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) );
-	}
-
-	// Previous/next page navigation.
-	twenty_twenty_one_the_posts_navigation();
-
-} else {
-
-	// If no content, include the "No posts found" template.
-	get_template_part( 'template-parts/content/content-none' );
-
-}
-
 get_footer();
+?>
