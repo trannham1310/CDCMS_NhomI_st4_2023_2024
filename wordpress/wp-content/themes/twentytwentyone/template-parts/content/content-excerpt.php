62eba7f06f1dd2fbfb7f58fd06411caa57d98523
nham1310 <trannham05042002@gmail.com>
2023-10-30 18:38:21 +0700

    60%

--- wordpress/wp-content/themes/twentytwentyone/template-parts/content/content-excerpt.php
+++ wordpress/wp-content/themes/twentytwentyone/template-parts/content/content-excerpt.php
@@ -13,56 +13,54 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="list_new_view" style="margin: 0 auto;">
-		<div class=" row">
-			<div class="col-md-3" style="padding: 35px">
-				<span class="topnewsdate">13</span>
-				<br />
-				<span class="topnewsmonth">Tháng 08</span>
-			</div>
-			<div class="col-md-7" style="padding: 35px">
-				<?php get_template_part('template-parts/header/excerpt-header', get_post_format()); ?>
+  <div class="list_new_view" style="margin: 0 auto;">
+    <div class=" row">
+      <div class="col-md-3" style="padding: 35px">
+        <span class="topnewsdate">13</span>
+        <br />
+        <span class="topnewsmonth">Tháng 08</span>
+      </div>
+      <div class="col-md-7" style="padding: 35px">
+        <?php get_template_part('template-parts/header/excerpt-header', get_post_format()); ?>
 
-				<div class="entry-content">
-					<?php get_template_part('template-parts/excerpt/excerpt', get_post_format()); ?>
-				</div><!-- .entry-content -->
-			</div>
-		</div>
-	</div>
-	<footer class="entry-footer default-max-width">
-		<?php twenty_twenty_one_entry_meta_footer(); ?>
-	</footer><!-- .entry-footer -->
+        <div class="entry-content">
+          <?php get_template_part('template-parts/excerpt/excerpt', get_post_format()); ?>
+        </div><!-- .entry-content -->
+      </div>
+    </div>
+  </div>
+
 </article><!-- #post-${ID} -->
 
 <style>
-	.list_new_view {
-		align-items: center;
-		background-color: #fdfdfd;
-		border: 1px solid black;
-		width: 550px;
-		height: 300px;
-	}
+.list_new_view {
+  align-items: center;
+  background-color: #fdfdfd;
+  border: 1px solid black;
+  width: 550px;
+  height: 300px;
+}
 
-	.topnewsdate {
-		font-size: 65px;
-		color: #000000;
-	}
+.topnewsdate {
+  font-size: 65px;
+  color: #000000;
+}
 
-	header-detail {
-		background-color: white;
-		color: blue;
-		font-size: 18px;
-		font-weight: bold;
-	}
+header-detail {
+  background-color: white;
+  color: blue;
+  font-size: 18px;
+  font-weight: bold;
+}
 
-	.row {
+.row {
 
-		display: flex;
-	}
+  display: flex;
+}
 
-	.col-md-3 {
-		margin-top: 10px;
-		margin-bottom: 10px;
-		border-right: 1px solid black;
-	}
+.col-md-3 {
+  margin-top: 10px;
+  margin-bottom: 10px;
+  border-right: 1px solid black;
+}
 </style>
