035b58623e2c8061289773ce5621279d26b3cefa
nham1310 <trannham05042002@gmail.com>
2023-10-31 11:26:01 +0700

    fix time x2

--- wordpress/wp-content/themes/twentytwentyone/single.php
+++ wordpress/wp-content/themes/twentytwentyone/single.php
@@ -14,11 +14,20 @@ get_header();
 
 /* Start the Loop */
 while (have_posts()) :
-	the_post();
-
   the_post();
 
   get_template_part('template-parts/content/content-single');
+
+  if (is_attachment()) {
+    // Parent post navigation.
+    the_post_navigation(
+      array(
+        /* translators: %s: Parent post link. */
+        'prev_text' => sprintf(__('<span class="meta-nav">Published in</span><span class="post-title">%s</span>', 'twentytwentyone'), '%title'),
+      )
+    );
+  }
+
   $twentytwentyone_next = is_rtl() ? twenty_twenty_one_get_icon_svg('ui', 'arrow_left') : twenty_twenty_one_get_icon_svg('ui', 'arrow_right');
   $twentytwentyone_prev = is_rtl() ? twenty_twenty_one_get_icon_svg('ui', 'arrow_right') : twenty_twenty_one_get_icon_svg('ui', 'arrow_left');
 
